57f1ea2640298b42cd2c19be24a38ef027f330ac
raklein <raklein22@gmail.com>
2019-03-05 17:55:46 +0100

    Removed middle analysis that's part of the new one Made sensible varible names and added comments

--- model_comparison.R
+++ model_comparison.R
@@ -1,6 +1,11 @@
 # Open the .Rproj file in RStudio to automatically setwd()
 # Otherwise set the working directory to the folder with this script in it.
 
+###############################################################################
+### This first part is the original code, Rick based his code off this      ###
+### for cross-validation tests. Can skip to around line 104 for that.       ###
+###############################################################################
+
 library(randomForest)
 library(party)
 
@@ -97,92 +102,6 @@ for (loop in 1:loops) {
 }
 
 ###############################################################################
-### Rick trying holdout validation with a loop across many seeds           ####
-### This is not quite k-folds                                              ####
-###############################################################################
-
-seeds <- 100
-
-# initialize objects
-variance_account1 <- NA
-variance_account2 <- NA
-variance_account3 <- NA
-
-for (seed in 1:seeds){
-  # Here's a simple method to randomize train and test samples. This
-  # exact method is modified from one DataCamp uses, but there are dozens
-  # of ways to code this same procedure.
-  # Set seed for reproducibility
-  set.seed(seed)
-  rows <- sample(nrow(data)) #creates randomized vector same length as data
-  data_randomized <- data[rows,] #randomizes df to index from 'rows'
-  split <- round(nrow(data)*.80) #creates index to split the file into 4/5 1/5, rounded
-  train <- data_randomized[1:split,] #first 4/5 to train
-  test <- data_randomized[(split+1):nrow(data),] #remaining 1/5 to test
-  
-  ##### fit the random forest model
-  forest_model <- randomForest(StroopEffect ~ ., 
-                               data = train, 
-                               importance = TRUE,
-                               ntree = 500,
-                               mtry = 5,
-                               replace = FALSE,
-                               sampsize = ceiling(.632 * nrow(train)))
-  
-  # estimate variable importance for rf
-  variable_importance <- importance(forest_model, type=1)
-  
-  # extract predictions
-  model_prediction <- predict(forest_model, newdata = test)
-  
-  # evaluate the model, pull out R^2
-  variance_account1[seed] <- cor(test$StroopEffect, model_prediction)^2
-  
-  ####### fit the conditional forest
-  forest_model <- cforest(StroopEffect ~ ., 
-                          data = train, 
-                          controls = cforest_control(teststat = "quad", 
-                                                     testtype = "Univ", 
-                                                     mincriterion = 0, 
-                                                     ntree = 500,
-                                                     mtry = 5,
-                                                     replace = FALSE,
-                                                     fraction = 0.632))
-  
-  # estimate variable importance for crf
-  variable_importance <- varimp(forest_model)
-  
-  # extract predictions
-  model_prediction <- predict(forest_model, newdata=test)
-  
-  # evaluate the model, pull out R^2
-  variance_account2[seed] <- as.numeric(cor(test$StroopEffect, model_prediction)^2)
-  
-  ######## Compare to linear regression
-  # Build the model predicting stroop from all other variables
-  model1 <- lm(data=train,StroopEffect~.)
-  summary(model1) # same model, so same results
-  
-  model_prediction <- predict(model1, newdata = test) # note the "test" dataset must have all the columns used in train
-  
-  # evaluate the model, pull out R^2
-  variance_account3[seed] <- cor(test$StroopEffect, model_prediction)^2
-}
-
-# Average r squared across the trials for
-# RF
-mean(variance_account1)
-# CRF
-mean(variance_account2)
-# Linear regression
-mean(variance_account3)
-
-merged_rsq_test <- data.frame(variance_account1,variance_account2,variance_account3)
-names(merged_rsq_test) <- c("rf","crf","lm")
-write.csv(merged_rsq_test, "onefifth_rsquared_test.csv")
-
-
-###############################################################################
 ### Rick comparing variable importance between train and test set          ####
 ###                                                                        ####
 ###############################################################################
@@ -197,16 +116,16 @@ names(data)[1] <- "StroopEffect"
 # remove missing rows
 data <- na.omit(data)
 
-seeds <- 10
+seeds <- 100
 
 # initialize objects
-variance_account1 <- NA
-variance_account2 <- NA
-variance_account3 <- NA
-variable_importance1 <- NA
-variable_importance2 <- NA
-variable_importance_test1 <- NA
-variable_importance_test2 <- NA
+variance_account_rf <- NA
+variance_account_crf <- NA
+variance_account_regression <- NA
+variable_importance_rf <- NA
+variable_importance_crf <- NA
+variable_importance_rf_test <- NA
+variable_importance_crf_test <- NA
 
 for (seed in 1:seeds){
   # Here's a simple method to randomize train and test samples. This
@@ -239,14 +158,14 @@ for (seed in 1:seeds){
                                sampsize = ceiling(.632 * nrow(test)))
   
   # estimate variable importance for rf
-  variable_importance1[seed] <- list(importance(forest_model, type=1))
-  variable_importance_test1[seed] <- list(importance(forest_model_test, type=1))
+  variable_importance_rf[seed] <- list(importance(forest_model, type=1))
+  variable_importance_rf_test[seed] <- list(importance(forest_model_test, type=1))
     
   # extract predictions
   model_prediction <- predict(forest_model, newdata = test)
   
   # evaluate the model, pull out R^2
-  variance_account1[seed] <- cor(test$StroopEffect, model_prediction)^2
+  variance_account_rf[seed] <- cor(test$StroopEffect, model_prediction)^2
   
   ####### fit the conditional forest train
   forest_model <- cforest(StroopEffect ~ ., 
@@ -270,14 +189,14 @@ for (seed in 1:seeds){
                                                      fraction = 0.632))
   
   # estimate variable importance for crf
-  variable_importance2[seed] <- list(varimp(forest_model))
-  variable_importance_test2[seed] <- list(varimp(forest_model_test))
+  variable_importance_crf[seed] <- list(varimp(forest_model))
+  variable_importance_crf_test[seed] <- list(varimp(forest_model_test))
   
   # extract predictions
   model_prediction <- predict(forest_model, newdata=test)
   
   # evaluate the model, pull out R^2
-  variance_account2[seed] <- as.numeric(cor(test$StroopEffect, model_prediction)^2)
+  variance_account_crf[seed] <- as.numeric(cor(test$StroopEffect, model_prediction)^2)
   
   ######## Compare to linear regression
   # Build the model predicting stroop from all other variables
@@ -287,39 +206,44 @@ for (seed in 1:seeds){
   model_prediction <- predict(model1, newdata = test) # note the "test" dataset must have all the columns used in train
   
   # evaluate the model, pull out R^2
-  variance_account3[seed] <- cor(test$StroopEffect, model_prediction)^2
+  variance_account_regression[seed] <- cor(test$StroopEffect, model_prediction)^2
 }
 
 # Average r squared across the trials for
 # RF
-mean(variance_account1)
+mean(variance_account_rf)
 # CRF
-mean(variance_account2)
+mean(variance_account_crf)
 # Linear regression
-mean(variance_account3)
+mean(variance_account_regression)
 
-merged_rsq_test <- data.frame(variance_account1,variance_account2,variance_account3)
+merged_rsq_test <- data.frame(variance_account_rf,variance_account_crf,variance_account_regression)
 names(merged_rsq_test) <- c("rf","crf","lm")
 write.csv(merged_rsq_test, "onefifth_rsquared_test.csv")
 
-vi1 <- as.data.frame(variable_importance1)
-vi_test1 <- as.data.frame(variable_importance_test1)
-vi2 <- as.data.frame(variable_importance2)
-vi_test2 <- as.data.frame(variable_importance_test2)
+# Tidying up - convert to data frames and change variable names
+variable_importance_rf <- as.data.frame(variable_importance_rf)
+variable_importance_crf <- as.data.frame(variable_importance_crf)
+variable_importance_rf_test <- as.data.frame(variable_importance_rf_test)
+variable_importance_crf_test <- as.data.frame(variable_importance_crf_test)
 
-names(vi1) <- paste("seed", as.character(rep(1:seed)), sep="")
-names(vi_test1) <- paste("seed", as.character(rep(1:seed)), sep="")
-names(vi2) <- paste("seed", as.character(rep(1:seed)), sep="")
-names(vi_test2) <- paste("seed", as.character(rep(1:seed)), sep="")
+names(variable_importance_rf) <- paste("seed", as.character(rep(1:seed)), sep="")
+names(variable_importance_crf) <- paste("seed", as.character(rep(1:seed)), sep="")
+names(variable_importance_rf_test) <- paste("seed", as.character(rep(1:seed)), sep="")
+names(variable_importance_crf_test) <- paste("seed", as.character(rep(1:seed)), sep="")
 
-rf_cor <- NA
-crf_cor <- NA
+# This is a loop that rank-order correlates the variable importance list for 
+# each analysis on the training set, with the corresponding list from the test
+# set.
+
+traintest_cor_rf <- NA
+traintest_cor_crf <- NA
 for(seed in 1:seeds){
-train_test_cor_rf <- cor.test(vi1[[seed]], vi_test1[[seed]], method="spearman")
-train_test_cor_crf <- cor.test(vi2[[seed]], vi_test2[[seed]], method="spearman")
-rf_cor[seed] <- train_test_cor_rf$estimate
-crf_cor[seed] <- train_test_cor_crf$estimate
+traintest_cor_rf_model <- cor.test(variable_importance_rf[[seed]], variable_importance_rf_test[[seed]], method="spearman")
+traintest_cor_crf_model <- cor.test(variable_importance_crf[[seed]], variable_importance_crf_test[[seed]], method="spearman")
+traintest_cor_rf[seed] <- traintest_cor_rf_model$estimate
+traintest_cor_crf[seed] <- traintest_cor_crf_model$estimate
 }
 
-mean(rf_cor)
-mean(crf_cor)
+mean(traintest_cor_rf)
+mean(traintest_cor_crf)
